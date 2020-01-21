16c7b76de8761d0adce2c41d4cc32299cef8e77e
shellfly <shell0fly@gmail.com>
2020-01-21 15:57:42 +0800

    fix multiline for read all strings, add merge sort

--- stdin/stdin.go
+++ stdin/stdin.go
@@ -7,9 +7,11 @@ import (
 )
 
 // ReadAllStrings returns all strings as a slice from the input
-func ReadAllStrings() []string {
+func ReadAllStrings() (words []string) {
 	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	line := scanner.Text()
-	return strings.Split(line, " ")
+	for scanner.Scan() {
+		line := scanner.Text()
+		words = append(words, strings.Split(line, " ")...)
+	}
+	return
 }
