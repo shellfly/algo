ce2fc6b483c932fd3939a7e5336ac3ae6e6d6d45
shellfly <shell0fly@gmail.com>
2020-02-02 15:40:25 +0800

    add bread first paths

---
+++ cmd/bread-first-paths/main.go
@@ -0,0 +1,72 @@
+/******************************************************************************
+ *  Execution:    go run cmd/bread-first-paths/main.go G s
+ *  Dependencies: Graph.java Queue.java Stack.java StdOut.java
+ *  Data files:   https://algs4.cs.princeton.edu/41graph/tinyCG.txt
+ *                https://algs4.cs.princeton.edu/41graph/tinyG.txt
+ *                https://algs4.cs.princeton.edu/41graph/mediumG.txt
+ *                https://algs4.cs.princeton.edu/41graph/largeG.txt
+ *
+ *  Run breadth first search on an undirected graph.
+ *  Runs in O(E + V) time.
+ *
+ *  %  go run cmd/graph/main.go tinyCG.txt
+ *  6 8
+ *  0: 2 1 5
+ *  1: 0 2
+ *  2: 0 1 3 4
+ *  3: 5 4 2
+ *  4: 3 2
+ *  5: 3 0
+ *
+ *  %  go run cmd/bread-first-paths/main.go tinyCG.txt 0
+ *  0 to 0 (0):  0
+ *  0 to 1 (1):  0-1
+ *  0 to 2 (1):  0-2
+ *  0 to 3 (2):  0-2-3
+ *  0 to 4 (2):  0-2-4
+ *  0 to 5 (1):  0-5
+ *
+ *  %  go run cmd/bread-first-paths/main.go largeG.txt 0
+ *  0 to 0 (0):  0
+ *  0 to 1 (418):  0-932942-474885-82707-879889-971961-...
+ *  0 to 2 (323):  0-460790-53370-594358-780059-287921-...
+ *  0 to 3 (168):  0-713461-75230-953125-568284-350405-...
+ *  0 to 4 (144):  0-460790-53370-310931-440226-380102-...
+ *  0 to 5 (566):  0-932942-474885-82707-879889-971961-...
+ *  0 to 6 (349):  0-932942-474885-82707-879889-971961-...
+ *
+ ******************************************************************************/
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/shellfly/algo"
+	"github.com/shellfly/algo/stdin"
+)
+
+func main() {
+	graph := algo.NewGraph(stdin.NewIn(os.Args[1]))
+	s, _ := strconv.Atoi(os.Args[2])
+	p := algo.NewBreadFirstPaths(graph, s)
+
+	for v := 0; v < graph.V(); v++ {
+		if p.HasPathTo(v) {
+			fmt.Printf("%d to %d: ", s, v)
+			for _, x := range p.PathTo(v) {
+				if x == s {
+					fmt.Print(x)
+				} else {
+					fmt.Printf("-%d", x)
+				}
+			}
+			fmt.Println()
+		} else {
+			fmt.Printf("%d and %d: not connected\n", s, v)
+		}
+	}
+
+}
